7eee19e18af4982e4e54057cac13cba339df4e63
maintenance <projectmaintenancemii@gmail.com>
2024-05-07 09:03:28 +0700

    UBAH LOGIN2

--- application/views/auth/auth_login.php
+++ application/views/auth/auth_login.php
@@ -52,7 +52,7 @@
                 <p class="login-box-msg" style="font-size:17px;">
                     <b>APPLICATION</b>
                     <br>
-                    <b> WAREHOUSE MAINTENANCE SYSTEM dsfwredscx</b>
+                    <b> WAREHOUSE MAINTENANCE SYSTEM</b>
                 </p>
 
                 <?= form_open('auth/verify_login', array('id' => 'form-login')); ?>
